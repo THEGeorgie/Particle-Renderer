0c976d63a16532e443478a907fc4b7acda738b8a
THEGeorgie <memesport140@gmail.com>
2022-09-29 20:25:20 +0200

    commit

--- Particle Renderer/main.cpp
+++ Particle Renderer/main.cpp
@@ -1,7 +1,8 @@
 #include <SDL.h>
 #include <stdio.h>
 #include <iostream> 
-#include "Screen.h"
+#include "Screen.h" 
+#include <math.h>
 using namespace gogi;
 using namespace std;
 
@@ -14,19 +15,27 @@ int main(int argc, char* args[]) {
 		cout << "Intialising failed" << endl;
 	}
 
-	
+	int max = 0;
 
 	while(true) {
 		// Update particles
 		
+
 		// Draw particles
+		int elapsed = SDL_GetTicks();
+
+		unsigned char red = (unsigned char)((1 + sin(elapsed * 0.0001)) * 128);
+		unsigned char green = (unsigned char) ((1 + cos(elapsed * 0.0002)) *128);
+		unsigned char blue = (unsigned char) ((1 + sin(elapsed * 0.0003)) * 128);
+
+		if (green > max) max = green;
+
 		for (int y = 0; y < Screen::screenH; y++) {
 			for (int x = 0; x < Screen::screenW; x++) {
-				screen.setPixle(x, y, 120, 0, 255);
+				screen.setPixle(x, y, red, green, blue);
 			}
 		}
 
-		screen.setPixle(400, 300, 255, 255, 255);
 
 		// Check for events
 		screen.update();
